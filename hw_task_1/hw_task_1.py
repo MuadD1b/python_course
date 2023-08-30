59d3c02eafde0631017318a449c42f3cb6869033
Sergey_Chishko <sergeychishko@gmail.com>
2023-08-30 20:09:19 +0300

    HW-1: task-3

--- hw_task_1/hw_task_1.py
+++ hw_task_1/hw_task_1.py
@@ -1,11 +1,11 @@
-def str_len(string):    # Задание 1
+def str_len(string: str) -> int:    # Задание 1
     if type(string) == str:
         return len(string)
     else:
         return 'Input must be string!'
 
 
-def list_len_compare(list1, list2): # Задание 2
+def list_len_compare(list1: list, list2: list) -> int: # Задание 2
     if type(list1) != list or type(list2) != list:
         return 'Inputs must be lists!'
 
@@ -15,5 +15,26 @@ def list_len_compare(list1, list2): # Задание 2
         return len(list2)
 
 
+def list_len_compare(list1: list, list2: list) -> int: # Задание 2
+    if type(list1) != list or type(list2) != list:
+        return 'Inputs must be lists!'
+
+    if len(list1) > len(list2):
+        return len(list1)
+    else:
+        return len(list2)
+
+
+def list_append(list1: list, value: any) -> list: # Задание 3
+    if type(list1) != list:
+        return 'Input 1 must be lists!'
+    else:
+        list1.append(value)
+        return list1
+
+
+
+
 print(str_len('56'))
 print(list_len_compare([5, 5, 5], ['y', 'u', 'p', 'i']))
+print(list_append([5, 5, 5], ['y', 'u', 'p', 'i']))
