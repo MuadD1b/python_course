37609f5b4c60cc09046f6a51f2543ed469d8dd3e
Sergey_Chishko <sergeychishko@gmail.com>
2023-09-02 22:12:36 +0300

    HW-2: task-2

--- hw_task_2/hw_task_2.py
+++ hw_task_2/hw_task_2.py
@@ -6,10 +6,12 @@ def remove(string: str) -> str: # задание 1
     return string
 
 
-
+def num_filter(num: int, to_filter:list) -> list:   # задание 2
+    return list(filter(lambda x: x % num == 0, to_filter))
 
 
 
 
 
 print(remove('GoGogo s fgGos s go phpC C Py'))
+print(num_filter(3, [3, 5, 6, 66, 22, 33, 3]))
