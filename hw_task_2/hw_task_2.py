0b06d0b96ccfa08a85638d5c61294f33017dc66d
Sergey_Chishko <sergeychishko@gmail.com>
2023-09-02 22:22:50 +0300

    HW-2: task-3

--- hw_task_2/hw_task_2.py
+++ hw_task_2/hw_task_2.py
@@ -10,8 +10,12 @@ def num_filter(num: int, to_filter:list) -> list:   # задание 2
     return list(filter(lambda x: x % num == 0, to_filter))
 
 
+def only_string(*args) -> tuple:    # задание 3
+    return tuple(filter(lambda x: isinstance(x, str), args))
+
 
 
 
 print(remove('GoGogo s fgGos s go phpC C Py'))
 print(num_filter(3, [3, 5, 6, 66, 22, 33, 3]))
+print(only_string('5', 5, 'ddf', 63.9, 'jg'))
