b7c92ac474b71d01d545b912fb2b092e649f4da1
Sergey_Chishko <sergeychishko@gmail.com>
2023-10-03 19:39:10 +0300

    HW-5: task-3

--- hw_task_5/hw_task_5.py
+++ hw_task_5/hw_task_5.py
@@ -13,3 +13,17 @@ if not df.empty:
     print(df)
 else:
     print('No line with all values > 5')
+
+
+m = np.random.randint(1000, size=100, dtype=int).reshape((10,10)) # задание 3
+df = pd.DataFrame(m)
+ind = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df.index = ind
+df.columns = ind
+print('Matrix shape is', df.shape)
+print('Matrix rows are', list(df.index))
+print('Matrix columns are', list(df.columns))
+print('Matrix mean is', df.mean().mean())
+df.to_csv('Matrix_task_3.csv')
+
+
