127a8b113e2c21873050f681545e32e1656a8847
Sergey_Chishko <sergeychishko@gmail.com>
2023-10-03 21:35:49 +0300

    HW-4: task-2

--- hw_task_4/hw_task_4.py
+++ hw_task_4/hw_task_4.py
@@ -5,3 +5,11 @@ m = np.random.randint(1, high=11, size=10, dtype=int) # задание 1
 print('task 1')
 print(sorted(m))
 print()
+
+
+x = np.zeros((8,8),dtype=int)   # задание 2
+print('task 2')
+x[1::2,::2] = 1
+x[::2,1::2] = 1
+print(x)
+print()
