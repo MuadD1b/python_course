43b95499a7b5f2044eec22389b9c4878537c0b87
Sergey_Chishko <sergeychishko@gmail.com>
2023-10-03 21:53:17 +0300

    HW-4: task-4

--- hw_task_4/hw_task_4.py
+++ hw_task_4/hw_task_4.py
@@ -20,3 +20,12 @@ n = np.random.randint(1, high=11, size=8, dtype=int).reshape(4, 2)
 print('task 3')
 print(np.dot(m, n))
 print()
+
+
+m = np.random.rand(10) # задание 4
+for i in range(len(m)):
+    while m[i] == 0:
+        m[i] = np.random.random()
+print('task 4')
+print(m)
+print()
